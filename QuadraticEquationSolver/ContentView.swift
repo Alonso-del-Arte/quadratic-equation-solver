0a5f359033220950b34e999594ed637d9015e0a3
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-20 14:43:28 -0400

    Initial Commit

---
+++ QuadraticEquationSolver/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  QuadraticEquationSolver
+//
+//  Created by Alonso del Arte on 10/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ QuadraticEquationSolver/QuadraticEquationSolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuadraticEquationSolverApp.swift
+//  QuadraticEquationSolver
+//
+//  Created by Alonso del Arte on 10/20/23.
+//
+
+import SwiftUI
+
+@main
+struct QuadraticEquationSolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
