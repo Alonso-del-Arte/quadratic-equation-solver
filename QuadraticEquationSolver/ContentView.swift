aec6a26483bf0399efa0ec85d75b914e1845c6aa
Alonso del Arte <alonso.delarte@gmail.com>
2023-10-20 23:16:56 -0400

    Some progress

--- QuadraticEquationSolver/ContentView.swift
+++ QuadraticEquationSolver/ContentView.swift
@@ -8,12 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var a: Int = 1
+    @State private var b: Int = -3
+    @State private var c: Int = 2
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Quadratic Equation Solver")
+                .font(.largeTitle)
+            HStack {
+                Text("a = ")
+                TextField("a", text: Binding(
+                    get: {String(a)},
+                    set: {a = Int($0) ?? 0}
+                ))
+                Text(", b = ")
+                TextField("b", text: Binding(
+                    get: {String(b)},
+                    set: {b = Int($0) ?? 0}
+                ))
+                Text(", c = ")
+                TextField("c", text: Binding(
+                    get: {String(c)},
+                    set: {c = Int($0) ?? 0}
+                ))
+            }
+            HStack {
+                Text("Roots of\n\(a)x^2 + \(b)x + \(c) are")
+                    .font(.largeTitle)
+            }
         }
         .padding()
     }
